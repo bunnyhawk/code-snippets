d3d6b930086902a86de9f9b17e78f1aea8fa929d
Robert Higdon <positronicshell@gmail.com>
2013-03-08 16:50:37 -0800

    Added iFrame/Modal extend

--- snippets.js
+++ snippets.js
@@ -45,3 +45,55 @@ $('a.replaceDiv').click(function(e) {
         $('.' + divTarget).load(fullLink).html();
     }       
 });
+
+/*
+    ===== Build an iFrame in jQuery Dialog/Modal =====
+    To build an iframe link to a modal window (after extending jQuery dialog to 'modal' class), use this code:
+    <a href="#example" class="modal iframe">Free Email</a>
+    <div id="example" style="display: none;" data-target="url-here" data-height="600" data-width="800"> </div>
+
+    If you want an auto-load dialog (with no link), use this code, making sure it has the "autoLoad" class:
+    <div id="example" class="autoLoad" style="display: none;" data-target="url-here" data-height="600" data-width="800"> </div>
+
+*/
+var buildIframe = function (href) {
+    var extLink = $(href).attr('data-target');
+    var iDivHeight = $(href).attr('data-height');
+    var iDivWidth = $(href).attr('data-width');
+
+    var builtIframe = $('<iframe />').attr({
+        'src': extLink,
+        'frameborder': 0,
+        'height': iDivHeight,
+        'width': iDivWidth
+    }).addClass('insidedialog');
+    $(href).append(builtIframe);
+}
+
+var iframeDialogOptions = function (div) {       
+    $('.ui-dialog').css({
+        'height': 'auto',
+        'width': 'auto'
+    });
+    $(div).dialog('option', 'position', 'center');
+    $('.ui-dialog-titlebar-close').click(function () {
+        $('iframe.insidedialog').remove();
+    });
+}
+
+// Build when iframe comes from a link
+$('.iframe').click(function () {
+    var href = $(this).attr('href');
+
+    buildIframe(href);
+    iframeDialogOptions(href);
+});
+// Build when iframe is setup as document.onload
+if ($('.autoLoad')[0]) {
+    var autoLoadTarget = $('.autoLoad').attr('id');
+    var href = '#' + autoLoadTarget;
+
+    buildIframe(href);
+    $(href).dialog();
+    iframeDialogOptions(href);
+}
